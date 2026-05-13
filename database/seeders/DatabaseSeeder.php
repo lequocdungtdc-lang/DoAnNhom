fbe778f7724e6d723f9cd3c26520c16026fd80a6
Trover204 <trover204@gmail.com>
2026-05-14 02:08:19 +0700

    crud sonng + seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
             SampleDataSeeder::class,
             PlanSeeder::class,
             SubscriptionSeeder::class,
+            SongSeeder::class,
         ]);
     }
 }
